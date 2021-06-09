0f01f32722457774135442c8c72c9534b0712ab6
Prasanth <prasanthmahendran04@gmail.com>
2021-06-09 16:18:59 +0530

    This is my first API application

---
+++ Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Datingapp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Datingapp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext( DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<AppUser> Users  { get; set; }
+    }
+}
