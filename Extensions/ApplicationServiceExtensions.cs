7323bb5217c226e2e15b823afa6fdb1cf735d8ce
Prasanth <prasanthmahendran04@gmail.com>
2021-06-14 19:22:37 +0530

    FirstApiApplication_v3

---
+++ Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Datingapp.Data;
+using Datingapp.Interface;
+using Datingapp.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Datingapp.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services,IConfiguration config)
+        {
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
+            return services;
+        }
+    }
+}
---
+++ Interface/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Datingapp.Entities;
+
+namespace Datingapp.Interface
+{
+    public interface ITokenService
+    {
+        string CreateToken(AppUser user);
+    }
+}
